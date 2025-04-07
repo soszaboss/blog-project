34724349c3175bf53927be04e1c97c0498c22dc8
soszaboss <moustoifa10@outlook.fr>
2025-04-07 00:39:33 +0000

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,36 @@
 import express from "express";
 
-const app=express();
-const PORT=3000;
+const app = express();
+
+const PORT = 3000;
+
 app.get("/", (req, res)=>{
     res.send("Hello World!")
 });
+
+// app.get("/", (req, res)=>{
+//     res.json(
+//         [
+//             {
+//               "PostID": 1,
+//               "Title": "Introduction à Express.js",
+//               "Content": "Express.js est un framework léger et rapide pour Node.js...",
+//               "Category": "Développement Web",
+//               "PublicationDate": "2024-03-25T10:00:00Z",
+//               "Tags": "Express, Node.js, API"
+//             },
+//             {
+//               "PostID": 2,
+//               "Title": "Les bases de MongoDB",
+//               "Content": "MongoDB est une base de données NoSQL orientée documents...",
+//               "Category": "Bases de données",
+//               "PublicationDate": "2024-03-24T15:30:00Z",
+//               "Tags": "MongoDB, NoSQL, Backend"
+//             }
+//           ]
+//     )
+// })
+
 app.listen(3000,()=>{
     console.log(`my app is running on url http://localhost:${PORT}`);
 });
